44726f9725a73242275781ea7a74852d7dc416d2
Evan Cowden <evan38109@gmail.com>
2012-07-17 21:25:11 -0500

    Enhanced testWithPhantomJs script with better documentation and configurable output directory

--- dev/testWithPhantomJs.js
+++ dev/testWithPhantomJs.js
@@ -1,8 +1,25 @@
+/*
+ * Original source:
+ * http://www.google.com/url?sa=t&rct=j&q=&esrc=s&source=web&cd=1&ved=0CFMQFjAA&url=http%3A%2F%2Fblog.danmerino.com%2Fcontinuos-integration-ci-for-javascript-jasmine-and-teamcity%2F&ei=5BwGUI6kLoec8gS98aH1Bw&usg=AFQjCNECxWuvPOLLzzYezUm97NNzLYX_GQ
+ */
 console.log('Loading a web page');
 var page = require("webpage").create();
-//This was tricky, this is the way to open LOCAL files
+
+if (phantom.args.length !== 2) {
+    console.log("Usage: \"phantomjs testWithPhantom.js testPagePath outputPath\"");
+}
+
 var url = phantom.args[0];
-url = url.replace("C:", "localhost").replace("\\", "/");
+var outputPath = phantom.args[1];
+/*
+ * Ugly but effective translation of Windows paths to something PhantomJS finds acceptable.  Necessary since
+ * my build box is windows and passes in paths accordingly.  Should not interfere with Unix builds.
+ */
+function escapePathForWindows(path) {
+    return path.replace("C:", "localhost").replace("\\", "/");
+}
+url = escapePathForWindows(url);
+outputPath = escapePathForWindows(outputPath);
 
 console.log("Test URL: " + url);
 phantom.viewportSize = {width: 800, height: 600};
@@ -22,7 +39,7 @@ page.open(url, function (status) {
     } else {
         //Using a delay to make sure the JavaScript is executed in the browser
         window.setTimeout(function () {
-            page.render("output.png");
+            page.render(outputPath + "/output.png");
             phantom.exit();
         }, 200);
     }
